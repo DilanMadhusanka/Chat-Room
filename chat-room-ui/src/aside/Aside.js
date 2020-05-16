1b1fdb04f617b28d0105c8290143aeae08086e4a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-16 10:46:54 +0530

    create Aside component and do some implement inside it

---
+++ chat-room-ui/src/aside/Aside.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
+import Avatar from '@material-ui/core/Avatar';
+import Tooltip from '@material-ui/core/Tooltip';
+
+class Aside extends React.Component {
+    render() {
+        return (
+            <aside>
+                {/* <div className="vr"></div> */}
+                <TextField
+                    id="search full-width"
+                    label="Search members"
+                    type="search"
+                    onChange={this.handleSearch}
+                    margin="normal"
+                />
+                <ul >
+                    <List component="nav">
+                        {this.state.roomNotification.map((notification, i) =>
+                            this.state.yousername.toLowerCase().trim() === notification.sender.split('~')[0].toLowerCase().trim()
+                                ? ""
+                                : <li key={i} onClick={this.handleOpenPrivateBox} data-value={notification.sender.split('~')[0].toLowerCase().trim()}>
+                                    <div>
+                                        <div>
+                                            <ListItem
+                                                key={i}
+                                                role={undefined}
+                                                dense
+                                                button >
+                                                <Avatar alt="User Image" src={userImage} />
+
+                                                <ListItemText primary={notification.sender.split('~')[0]}
+                                                    secondary={notification.status === 'online' ||
+                                                        notification.status === 'typing...' ?
+                                                        <span className="status green"></span> : <span className="status orange"></span>} />
+                                                <ListItemSecondaryAction>
+                                                    <Tooltip title="Send private message">
+                                                        <IconButton aria-label="Private Message">
+                                                            <CommentIcon />
+                                                        </IconButton>
+                                                    </Tooltip>
+                                                </ListItemSecondaryAction>
+                                            </ListItem>
+                                        </div>
+                                        <br />
+
+                                    </div>
+                                </li>
+                        )} </List></ul>
+            </aside>
+        )
+    }
+}
+
+export default Aside
