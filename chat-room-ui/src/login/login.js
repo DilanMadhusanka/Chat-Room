4049b07ddefda2b6c7e8fd0293ad329586ce2f4a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-05-18 13:38:36 +0530

    add some sementic ui to project

--- chat-room-ui/src/login/login.js
+++ chat-room-ui/src/login/login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Login.css'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Card } from '@material-ui/core';
 export default class Login extends Component {
     constructor(props) {
         super(props);
@@ -23,18 +24,21 @@ export default class Login extends Component {
 
     render() {
         return (
-            <div className="component-Login">
-                <TextField
-                    id="user"
-                    label="Type your username"
-                    placeholder="Username"
-                    onChange={this.handleUserNameChange}
-                    margin="normal"
-                />
-                <br />
-                <Button variant="contained" color="primary" onClick={this.handleConnectPublicly} >
-                    Start Chatting
+            <div className="ui center aligned container">
+                <br/><br/><br/>
+                <Card className="ui segment" variant="outlined">
+                    <TextField
+                        id="user"
+                        label="Type your username"
+                        placeholder="Username"
+                        onChange={this.handleUserNameChange}
+                        margin="normal"
+                    />
+                    <br />
+                    <Button variant="contained" color="primary" onClick={this.handleConnectPublicly} >
+                        Start Chatting
              </Button>
+                </Card>
 
             </div>
         )
